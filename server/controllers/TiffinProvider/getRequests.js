15fbbb828848264d0ecf0a55c36331be75105566
Har-sh-arma <harinathsharma303@gmail.com>
2023-08-27 20:28:16 +0530

    Added Framework BoilerPlate for TiffinWala API

---
+++ server/controllers/TiffinProvider/getRequests.js
@@ -0,0 +1,8 @@
+const async_wrapper = require('../../middleware/async_wrapper'); 
+const Tiffin_provider = require('../../models/Tiffin_provider');
+const CUSTOMER = require('../../models/customer');
+
+const getRequests = async_wrapper(async(req, res)=>{
+})
+
+module.exports={getRequests} 
